73e674a1edea376ef1677c9428b2f3eec62aafcd
Usuario <Usuario@DESKTOP-605R33K>
2016-09-11 16:41:14 -0500

    primer commit

---
+++ src/acitvity/Acitvity.java
@@ -0,0 +1,25 @@
+
+package acitvity;
+
+/**
+ *
+ * @author Anyi Palma
+ */
+public class Acitvity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int num=4;
+        
+        if (num%2==0){
+        System.out.println("numero par");
+        }
+          else{
+              System.out.println("numero impar");
+          }
+ }
+}
